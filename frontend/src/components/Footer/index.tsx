1b90e5bc3886bfa2f305699eec971b126677034d
Leandro Pinto <leandrommpinto@gmail.com>
2021-09-06 23:49:40 +0100

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer mt-auto py-3 bg-dark">
+                <div className="container">
+                    <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/leandro-pinto-9116a39a/" target="_blank" rel="noreferrer">Leandro Pinto</a></p>
+                    <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                        Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
